edeedf7c9b6335bd305d3fdf743d3122c9ab84bc
InWoo <dlsdnghkd0503@naver.com>
2024-02-29 03:41:00 +0900

    싱글톤 패턴

---
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,32 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        // 순수한 DI 컨테이너는 싱글톤이 지켜지지 않는다.
+        // 여러명의 사용자가 요청을 보내면 그때마다 새로운 객체를 생성하여 굉장히 비효율 적이다.
+        // -> 메모리 낭비가 심하다.
+
+        AppConfig appConfig = new AppConfig();
+        // 1. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        // 2. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        // 참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 != memberService2
+        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+    }
+}
