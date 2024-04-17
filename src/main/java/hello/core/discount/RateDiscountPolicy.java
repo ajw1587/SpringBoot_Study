bc52dbc95bb567af5b7a4ffebd5318fd44b2e7a0
InWoo <dlsdnghkd0503@naver.com>
2024-04-18 02:16:38 +0900

    조회한 빈 모두 필요할때

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,9 +2,14 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Qualifier("mainDiscountPolicy")  // Bean 등록을 진행할때 자식 클래스도 다 가져와서 어떤 타입인지 파악을 하기 때문에 @Qualifier 로 타입을 명시해줄 수 있다.
+// 자체 Annotation을 만든다면, @MainDiscountPolicy 를 사용하면 된다.
+@Primary // 빈 등록에 있어서 우선 순위를 정해준다.
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -7,23 +7,25 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor // final 이 붙은 변수를 파라미터로 받는 생성자를 생성해준다.
+//@RequiredArgsConstructor // final 이 붙은 변수를 파라미터로 받는 생성자를 생성해준다.
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-//    @Autowired
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        System.out.println("memberRepository = " + memberRepository);
-//        System.out.println("discountPolicy = " + discountPolicy);
-//
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    // Bean 등록을 진행할때 자식 클래스도 다 가져와서 어떤 타입인지 파악을 하기 때문에 @Qualifier 로 타입을 명시해줄 수 있다.
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        System.out.println("memberRepository = " + memberRepository);
+        System.out.println("discountPolicy = " + discountPolicy);
+
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
